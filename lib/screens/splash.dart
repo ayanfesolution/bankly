c1163dcd5c39f426a7918e62aaf85bcda72ec04d
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2023-06-09 04:28:57 +0100

    project archetechture set up

---
+++ lib/screens/splash.dart
@@ -0,0 +1,68 @@
+
+
+
+import 'dart:async';
+
+import 'package:auto_adjust/auto_adjust.dart';
+import 'package:bankly/screens/trasactions/transaction_page.dart';
+import 'package:bankly/utils/navigations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class SplashScreen extends StatefulHookWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  static const String id = 'splash.screen';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+
+  @override
+  void initState(){
+    initialAction();
+    super.initState();
+  }
+
+  Future<Timer> initialAction() async {
+    return Timer(
+      const Duration(seconds: 4),
+      () {
+        RouteNavigators.routeReplace(context, const TransactionPage());
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AnimationController controller = useAnimationController(
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    late final Animation<double> animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeOutBack,
+    );
+    SizeConfig().init(context, 896, 386);
+    return Scaffold(
+    
+      body: Center(
+        child: SizedBox(
+          height: autoAdjustHeight(250),
+          width: autoAdjustWidth(250),
+          child: ScaleTransition(
+            scale: animation,
+            child: Image.asset(
+              'assets/images/mainlogo.png',
+              height: autoAdjustHeight(85),
+              width: autoAdjustWidth(240),
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/trasactions/transaction_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class TransactionPage extends StatefulHookWidget {
+  const TransactionPage({super.key});
+
+  @override
+  State<TransactionPage> createState() => _TransactionPageState();
+}
+
+class _TransactionPageState extends State<TransactionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
